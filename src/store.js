8cc75aa3dc3f3fe70425e47aa1cfe93d5d5eae3a
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-08-25 15:59:57 +0430

    add store for redux

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducer from './reducers'
+
+const middleware = [thunk];
+middleware.push(createLogger());
+
+const store = createStore(
+   reducer,
+   applyMiddleware(...middleware)
+);
+export default store;
