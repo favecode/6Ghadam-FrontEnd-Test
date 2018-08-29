ac52fef2b9517ce97b97f1311e04ee164baa2ee7
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-08-29 09:50:23 +0430

    add loginUser function in api service

--- src/api/service.js
+++ src/api/service.js
@@ -1,2 +1,21 @@
 import { serverConstants } from './../constants'
 import axios from 'axios'
+export const userService = {
+   loginUser
+}
+function loginUser(email, password) {
+   let url =
+      serverConstants.PROTOCOL +
+      serverConstants.AAA +
+      serverConstants.WEBSERVICE_URL +
+      serverConstants.CLIENTS +
+      serverConstants.LOGIN
+   return axios.post(url,
+      {
+         "realm": "Organization",
+         "email": email,
+         "password": password
+      })
+      .then(response => { return response })
+      .catch(err => console.log(err))
+}
