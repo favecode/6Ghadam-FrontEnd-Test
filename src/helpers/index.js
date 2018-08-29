e4e876440858b8821a7b22472dec7f1eb74114d3
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-08-29 10:24:44 +0430

    add readLeagues function in helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,11 +1,20 @@
 import { userService } from './../api/service'
 
 export const apiFunctions = {
-   loginUser
+   loginUser,
+   readLeagues
 }
 
 function loginUser(email, password) {
    userService.loginUser(email, password)
+      .then(
+         (response) => {
+            console.log(response)
+         }
+      )
+}
+function readLeagues(token) {
+   userService.readLeagues(token)
       .then(
          (response) => {
             console.log(response)
