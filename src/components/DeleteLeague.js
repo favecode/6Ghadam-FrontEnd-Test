d0d1dbbebb33607d4ef50bcd05866ab88157261e
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-08-29 09:09:28 +0430

    add DeleteLeague component

---
+++ src/components/DeleteLeague.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+class Leagues extends Component {
+   render() {
+      return (
+         <div>Delete Leagues</div>
+      );
+   }
+}
+export default Leagues;
