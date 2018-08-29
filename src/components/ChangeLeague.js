75e3a140d6d03cfec6a701c2f0dc5939c5250ada
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-08-29 09:06:28 +0430

    add ChangeLeague component

---
+++ src/components/ChangeLeague.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+class Leagues extends Component {
+   render() {
+      return (
+         <div>Change Leagues</div>
+      );
+   }
+}
+export default Leagues;
