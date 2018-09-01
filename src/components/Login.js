008ac5464acba412b10d8c8c1831d01bf94d7b71
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-09-01 11:11:36 +0430

    add handleChange function to Login component for store TextField value

--- src/components/Login.js
+++ src/components/Login.js
@@ -60,6 +60,16 @@ const styles = theme => ({
 
 // Components
 class Login extends Component {
+   state = {
+      email: '',
+      password: ''
+   }
+   handleChange = (element) => {
+      let { value , dataset } = element.target
+      this.setState({
+         [dataset.type] : value
+      })
+   }
    render() {
       const { classes } = this.props
       return (
@@ -68,11 +78,11 @@ class Login extends Component {
                <Grid item sm={4} className={classes.GridItem}>
                   <img src={logo} width="150" />
                   <Paper className={classes.Paper}>
-                     <Typography variant="display1" className={classes.Header} align="center">
+                     <Typography variant="title" className={classes.Header} align="center">
                         {textsConstants.LoginHeader}
                      </Typography>
-                     <TextField fullWidth margin="normal" className={classes.Input} placeholder={textsConstants.EmailInput}/>
-                     <TextField fullWidth margin="normal" className={classes.Input} type="password" placeholder={textsConstants.PasswordInput}/>
+                     <TextField fullWidth margin="normal" inputProps={{ 'data-type': 'email' }} onChange={this.handleChange} className={classes.Input} placeholder={textsConstants.EmailInput}/>
+                     <TextField fullWidth margin="normal" inputProps={{ 'data-type': 'password' }} onChange={this.handleChange} className={classes.Input} type="password" placeholder={textsConstants.PasswordInput}/>
                      <Button onClick={this.handleClick} size="small" className={classes.Button} variant="contained" value="Login">
                         {textsConstants.LoginButton}
                      </Button>
