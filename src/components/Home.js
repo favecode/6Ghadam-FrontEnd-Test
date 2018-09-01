6bfbc42e9b1e44322850c046beaaa6b61db0ff4d
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-09-01 17:16:14 +0430

    delete Tabs in Homes

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,22 +14,11 @@ import AddLeague from './AddLeague'
 import DeleteLeague from './DeleteLeague'
 import ChangeLeague from './ChangeLeague'
 
-// Icon
-import { 
-   Info,
-   Add,
-   Delete,
-   Edit
-} from '@material-ui/icons';
-
-
 // Material UI
 import { withStyles } from '@material-ui/core/styles';
 import {
    Grid,
    Paper,
-   Tabs,
-   Tab
 } from '@material-ui/core'
 
 // Styles
@@ -45,18 +34,9 @@ const styles = theme => ({
 
 // Component
 class Home extends Component {
-      state = {
-            activeItem : 0
-      }
-      handleClick = (activeItem) => {
-            this.setState({
-                  activeItem
-            })
-      }
       render() {
             const { token } = this.props.store
             const { classes } = this.props
-            const { activeItem } =  this.state
             return (
             <div>
                   {token == null 
@@ -67,25 +47,7 @@ class Home extends Component {
                   <Grid container dir="rtl" justify="center" alignContent="center" >
                         <Grid item sm={6}>
                         <Paper item className={classes.Paper}>
-                              <Tabs
-                              fullWidth
-                              indicatorColor="primary"
-                              textColor="primary"
-                              value={0}
-                              >
-                              <Tab label={textsConstants.LeaguesLink} icon={<Info/>} 
-                                    className={classes.Tab} onClick={() => {this.handleClick(0)}}/>
-                              <Tab label={textsConstants.CreateLink} icon={<Add />} 
-                                    className={classes.Tab} onClick={() => {this.handleClick(1)}}/>
-                              <Tab label={textsConstants.DeleteLink} icon={<Delete />} 
-                                    className={classes.Tab} onClick={() => {this.handleClick(2)}}/>
-                              <Tab label={textsConstants.ChangeLink} icon={<Edit/>} 
-                                    className={classes.Tab} onClick={() => {this.handleClick(3)}}/>
-                              </Tabs>
-                              {activeItem == 0 && <Leagues token={token}/>}
-                              {activeItem == 1 && <AddLeague token={token}/>}
-                              {activeItem == 2 && <DeleteLeague token={token}/>}
-                              {activeItem == 3 && <ChangeLeague token={token}/>}
+                              <Leagues token={token}/>
                         </Paper>
                         </Grid>
                   </Grid>
