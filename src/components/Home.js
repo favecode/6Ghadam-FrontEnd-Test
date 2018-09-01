335b2ae0c003bb5b3675973ade652765579cbf63
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-09-01 09:10:26 +0430

    import some components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,26 @@
-import React, { Component } from 'react'
+// React
+import React, { Component, Fragment } from 'react'
+
+// Redux
+import { connect } from 'react-redux'
+
+// Router
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+// Components
+import Login from './Login'
+import Leagues from './Leagues'
+import AddLeague from './AddLeague'
+import DeleteLeague from './DeleteLeague'
+import ChangeLeague from './ChangeLeague'
+
+// Material UI
+import {
+   Grid,
+   Paper,
+} from '@material-ui/core'
+
+// Component
 class Home extends Component {
    render() {
       return (
