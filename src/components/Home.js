297ab8a2ea8b8fbe9452be12f214cc559af725d3
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-09-01 13:41:42 +0430

    add Tabs to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,10 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 
 // Router
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+// Constants
+import { textsConstants } from './../constants'
 
 // Components
 import Login from './Login'
@@ -14,16 +17,40 @@ import AddLeague from './AddLeague'
 import DeleteLeague from './DeleteLeague'
 import ChangeLeague from './ChangeLeague'
 
+// Icon
+import { 
+   Info,
+   Add,
+   Delete,
+   Edit
+} from '@material-ui/icons';
+
+
 // Material UI
+import { withStyles } from '@material-ui/core/styles';
 import {
    Grid,
    Paper,
+   Tabs,
+   Tab
 } from '@material-ui/core'
+
+// Styles
+const styles = theme => ({
+   Tab : {
+      paddingTop: '1rem',
+      fontFamily : 'iranSans',
+   },
+   Paper : {
+      
+   }
+})
 
 // Component
 class Home extends Component {
    render() {
       const { token } = this.props.store
+      const { classes } = this.props
       return (
          <Router>
             {token == null 
@@ -33,18 +60,29 @@ class Home extends Component {
             <Fragment>
                <Grid container dir="rtl" justify="center" alignContent="center" >
                   <Grid item sm={6}>
-                     <Paper item>
-                           <Link to="/leagues">Leagues</Link>
-                           <Link to="/addLeague">Create League</Link>
-                           <Link to="/deleteLeague">Delete League</Link>
-                           <Link to="/changeLeague">Change League</Link>
+                     <Paper item className={classes.Paper}>
+                        <Tabs
+                           fullWidth
+                           indicatorColor="primary"
+                           textColor="primary"
+                           value={0}
+                        >
+                           <Tab label={textsConstants.LeaguesLink} icon={<Info/>} 
+                              className={classes.Tab} data-link="leagues" onClick={this.handleClick}/>
+                           <Tab label={textsConstants.CreateLink} icon={<Add />} 
+                              className={classes.Tab} data-link="addLeague" onClick={this.handleClick}/>
+                           <Tab label={textsConstants.DeleteLink} icon={<Delete />} 
+                              className={classes.Tab} data-link="deleteLeague" onClick={this.handleClick}/>
+                           <Tab label={textsConstants.ChangeLink} icon={<Edit/>} 
+                              className={classes.Tab} data-link="changeLeague" onClick={this.handleClick}/>
+                        </Tabs>
+                        <Route path='/leagues' exact={true} component={Leagues} />
+                        <Route path='/addLeague' component={AddLeague} />
+                        <Route path='/deleteLeague' component={DeleteLeague} />
+                        <Route path='/changeLeague' component={ChangeLeague} />
                      </Paper>
                   </Grid>
                </Grid>
-               <Route path='/leagues' exact={true} component={Leagues}/>
-               <Route path='/addLeague' component={AddLeague}/>
-               <Route path='/deleteLeague' component={DeleteLeague}/>
-               <Route path='/changeLeague' component={ChangeLeague}/>
             </Fragment>}
          </Router>
       );
@@ -57,4 +95,4 @@ const mapStateToProps = (state) => {
    }
 }
 
-export default connect(mapStateToProps,null)(Home)
+export default withStyles(styles)(connect(mapStateToProps, null)(Home)) 
