fb5adb22b4f37b94ab40671efdd6edc0aaa4500a
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-09-01 10:15:11 +0430

    import some components to Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,34 @@
-import React , { Component } from 'react'
+// React
+import React, { Component, Fragment } from 'react'
+
+// Redux
+import { connect } from 'react-redux'
+
+// Helpers
+import { apiFunctions } from './../helpers'
+
+// Constants 
+import { textsConstants } from './../constants'
+
+// Public
+import logo from './../public/img/logo.png'
+
+// Material UI
+import { withStyles } from '@material-ui/core/styles';
+import {
+   TextField,
+   Button,
+   Paper,
+   Grid,
+   Typography,
+} from '@material-ui/core';
+
+// Components
 class Login extends Component {
-   render(){
-      return(
+   render() {
+      return (
          <div>Login</div>
       );
    }
 }
-export default Login;
+export default withStyles(styles)(connect()(Login)) 
