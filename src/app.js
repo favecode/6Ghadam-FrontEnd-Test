e7a35c6a285aa81f1160fbe2c0516ebc091ecce3
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-09-01 10:42:12 +0430

    add font.css to app component

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import store from './store'
 import { BrowserRouter } from 'react-router-dom'
 import Home from './components/Home'
+import './public/font.css'
 export default class App extends Component {
    render(){
       return(
