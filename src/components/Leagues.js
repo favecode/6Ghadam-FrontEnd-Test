874ac3ddcc6caecc57594c79335df8ffa0deb35e
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-09-02 10:06:02 +0430

    add components and functions in Leagues component

--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -7,6 +7,18 @@ import { apiFunctions } from './../helpers'
 // Constants 
 import { textsConstants } from './../constants'
 
+// Icon
+import {
+   AddCircle,
+   Delete,
+   Edit
+} from '@material-ui/icons';
+
+// Components
+import AddLeague from './AddLeague'
+import DeleteLeague from './DeleteLeague'
+import ChangeLeague from './ChangeLeague'
+
 // Material UI
 import { withStyles } from '@material-ui/core/styles'
 import {
@@ -14,7 +26,8 @@ import {
    TableBody,
    TableCell,
    TableRow,
-   Typography
+   Typography,
+   TextField,
 } from '@material-ui/core'
 
 // Styles
@@ -25,42 +38,115 @@ const styles = theme => ({
       textAlign: 'center'
    },
    TableCell : {
-      fontFamily: 'iranSans', 
-      textAlign: 'right'     
+      fontFamily: 'iranSans',
+      textAlign: 'right',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center'  
+   },
+   DeleteIcon: {
+      color: 'rgba(0, 0, 0, 0.54)',
+      cursor: 'pointer'
+   },
+   EditIcon: {
+      color: 'rgba(0, 0, 0, 0.54)',
+      cursor: 'pointer',
+      margin:'auto .5rem'
+   },
+   Input : {
+      display : 'flex',
+      margin: '1rem 1rem',
+      justifyContent : 'space-between',
+      alignItems : 'center'
+   },
+   AddCircle : {
+      color: 'rgba(0, 0, 0, 0.54)',
+      cursor: 'pointer',
    }
 })
 
 // Components
 class Leagues extends Component {
    state = {
-      leagues : null
+      leagues : null,
+      Add : false,
+      Change : false,
+      Delete : false,
+      id : '',
+      name : ''
    }
    componentDidMount(){
+      this.Read();
+   }
+   Read = () => {
       const { token } = this.props
       apiFunctions.readLeagues(token).then(response => {
          this.setState({
-            leagues: response.data
+            leagues: response.data,
+            Change: false,
+            Add: false,
+            Delete: false,
          })
+      })
+   }
+   Change = (id,name) => {
+      this.setState({
+         Change: true,
+         Add: false,
+         Delete: false,
+         id,
+         name
+      })
+   }
+   Delete = (id) => {
+      this.setState({
+         Delete: true,
+         Change: false,
+         Add: false,
+         id
+      })
+   }
+   Add = () => {
+      this.setState({
+         Add : true,
+         Change : false,
+         Delete : false
       })
    }
    render() {
       const { leagues } = this.state
-      const { classes } = this.props
+      const { classes , token } = this.props
       return (
          <div>
             {leagues 
             ?
-               <Table>
-                  <TableBody>
-                     {Object.values(leagues).map(league =>
-                        <TableRow>
-                           <TableCell className={classes.TableCell}>
-                              {league.name}
-                           </TableCell>
-                        </TableRow>
-                     )}
-                  </TableBody>
-               </Table>
+               <Fragment>
+                  <AddLeague token={token} open={this.state.Add} read={this.Read}/>
+                  <DeleteLeague token={token} id={this.state.id} read={this.Read} open={this.state.Delete}/>
+                  <ChangeLeague token={token} id={this.state.id} read={this.Read} 
+                  name={this.state.name} open={this.state.Change}/>
+                  <div className={classes.Input}>
+                     <Typography variant="display1" className={classes.Typography} >
+                        {textsConstants.Leagues}
+                     </Typography>
+                     <AddCircle className={classes.AddCircle} onClick={this.Add}/>
+                  </div>
+                  <Table>
+                     <TableBody>
+                        {Object.values(leagues).map(league =>
+                           <TableRow>
+                              <TableCell className={classes.TableCell}>
+                                 {league.name}
+                                 <div>
+                                    <Edit className={classes.EditIcon} onClick={() => { this.Change(league.id,league.name) }} />
+                                    <Delete className={classes.DeleteIcon} onClick={() => { this.Delete(league.id) }} />
+                                 </div>
+                              </TableCell>
+                           </TableRow>
+                        )}
+                     </TableBody>
+                  </Table>
+               </Fragment>
             :
             <Typography className={classes.Typography}>
                {textsConstants.fetchData}
