c7394a899a7cf0e795fd7ec6866d8af75c7d57e6
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-09-02 10:08:16 +0430

    add content and style and function in ChangeLeague component

--- src/components/ChangeLeague.js
+++ src/components/ChangeLeague.js
@@ -1,9 +1,123 @@
+// React
 import React, { Component } from 'react'
-class Leagues extends Component {
-   render() {
-      return (
-         <div>Change Leagues</div>
-      );
-   }
+
+// Helpers
+import { apiFunctions } from './../helpers'
+
+// Constants
+import { textsConstants } from './../constants'
+
+// Material UI
+import { withStyles } from '@material-ui/core/styles'
+import {
+   Dialog,
+   DialogActions,
+   DialogContent,
+   DialogContentText,
+   DialogTitle,
+   TextField,
+   Button,
+   Typography
+} from '@material-ui/core'
+
+// Styles
+const styles = theme => ({
+      Dialog: {
+            fontFamily: 'iranSans'
+      },
+      DialogContent: {
+            fontFamily: 'iranSans'
+
+      },
+      DialogContentText: {
+            fontFamily: 'iranSans',
+            textAlign: 'right',
+            marginBottom : '1rem'
+      },
+      DialogTitle: {
+            fontFamily: 'iranSans',
+            textAlign: 'right'
+      },
+      Button : {
+            fontFamily: 'iranSans'
+      },
+      Input: {
+            fontFamily: 'iranSans',
+            textAlign: 'right'
+      }
+})
+
+// Components
+class ChangeLeague extends Component {
+      state = {
+            open: this.props.open,
+            name: this.props.name,
+
+      }
+
+      componentWillReceiveProps(next) {
+            this.setState({
+                  open: next.open,
+                  name: next.name
+            })
+      }
+
+      handleClickOpen = () => {
+            this.setState({ open: true })
+      }
+
+      handleClose = () => {
+            this.setState({ open: false })
+      }
+
+      handleChange = (element) => {
+            const { value } = element.target
+            this.setState({
+                  name: value
+            })
+      }
+
+      handleDone = () => {
+            const { token , id } = this.props
+            const { name } = this.state
+            if (name != '') {
+                  apiFunctions.updateLeague(token, name, '',id).then(response => {
+                        this.handleClose()
+                        this.props.read()
+                  })
+            }
+      }
+      render() {
+            const { classes } = this.props
+            return (
+            <Dialog
+                  open={this.state.open}
+                  onClose={this.handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+            >
+                  <DialogTitle id="alert-dialog-title">
+                        <Typography variant="title" className={classes.DialogTitle}>
+                              {textsConstants.Change}
+                        </Typography>
+                  </DialogTitle>
+                  <DialogContent>
+                        <DialogContentText id="alert-dialog-description" className={classes.DialogContentText}>
+                              {textsConstants.ChangeMessage}
+                        </DialogContentText>
+                              <TextField dir="rtl" onChange={this.handleChange} 
+                              defaultValue={this.state.name} className={classes.Input} />
+                  </DialogContent>
+                  <DialogActions className={classes.DialogActions}>
+                        <Button onClick={this.handleClose} color="primary" className={classes.Button}>
+                              {textsConstants.Cancel}
+                        </Button>
+                        <Button onClick={this.handleDone} color="primary" className={classes.Button}>
+                              {textsConstants.Confirm}
+                        </Button>
+                  </DialogActions>
+            </Dialog>
+            )
+      }
 }
-export default Leagues;
+export default withStyles(styles)(ChangeLeague)
