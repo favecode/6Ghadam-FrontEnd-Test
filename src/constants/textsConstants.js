54bfeec2375beae9e6e25487c520f9ca38b3cc4f
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-09-02 10:02:48 +0430

    add some const in Constants

--- src/constants/textsConstants.js
+++ src/constants/textsConstants.js
@@ -3,12 +3,17 @@ export const textsConstants = {
    LoginHeader: 'صفحه ورود',
    EmailInput: 'ایمیل',
    PasswordInput: 'رمز عبور',
-   LoadingHeader: 'لطفا منتطر بمانید ...',
+   LoadingHeader: 'لطفا منتظر بمانید ...',
    LeaguesLink: 'مشاهده',
-   CreateLink: 'اضافه',
-   DeleteLink: 'حذف',
-   ChangeLink: 'تغییر',
+   Create: 'اضافه کردن لیگ',
+   Delete: 'حذف کردن لیگ',
+   Change: 'تغییر نام لیگ',
    fetchData: 'در حال بارگذاری ...',
-   Leagues: 'لیگ ها'
+   Leagues: 'لیگ ها',
+   Cancel : 'انصراف',
+   Confirm : 'تایید',
+   DeleteMessage : 'آیا می خواهید لیگ انتخاب شده را حذف کنید؟',
+   ChangeMessage : 'نام جدید لیگ را وارد کنید',
+   CreateMessage : 'نام لیگ را وارد کنید',
 }
 
